351342d833d67386a9462317a388128b437bb80b
DevLomoSE <jonathancggs@gmail.com>
2021-09-14 10:46:38 -0500

    :sparkles: Cliente
    Adding findAll method to the client DAO implementation

--- src/main/java/com/devlomose/springboot/data/jpa/app/models/dao/ClienteDAOImplement.java
+++ src/main/java/com/devlomose/springboot/data/jpa/app/models/dao/ClienteDAOImplement.java
@@ -1,8 +1,27 @@
 package com.devlomose.springboot.data.jpa.app.models.dao;
 
+import com.devlomose.springboot.data.jpa.app.models.entity.Cliente;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
 /**
  * ClienteDAOImplement at: src/main/java/com/devlomose/springboot/data/jpa/app/models/dao
  * Created by @DevLomoSE at 14/9/21 10:39.
  */
-public class ClienteDAOImplement {
+@Repository
+public class ClienteDAOImplement implements ClienteDAO{
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @SuppressWarnings("unchecked")
+    @Transactional(readOnly = true)
+    @Override
+    public List<Cliente> findAll() {
+        return em.createQuery("from Cliente").getResultList();
+    }
 }
