85c2b7115d42040dac855af1da382b1dffd97b8d
DevLomoSE <jonathancggs@gmail.com>
2021-09-14 10:41:33 -0500

    :package: ClienteDAO
    Create the client DAO Implementation class

---
+++ src/main/java/com/devlomose/springboot/data/jpa/app/models/dao/ClienteDAOImplement.java
@@ -0,0 +1,8 @@
+package com.devlomose.springboot.data.jpa.app.models.dao;
+
+/**
+ * ClienteDAOImplement at: src/main/java/com/devlomose/springboot/data/jpa/app/models/dao
+ * Created by @DevLomoSE at 14/9/21 10:39.
+ */
+public class ClienteDAOImplement {
+}
