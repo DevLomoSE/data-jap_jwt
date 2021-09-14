9353b6febbba1d26e747bef5f8972cbf492167f7
DevLomoSE <jonathancggs@gmail.com>
2021-09-14 10:41:00 -0500

    :package: Cliente
    Create the client model class

---
+++ src/main/java/com/devlomose/springboot/data/jpa/app/models/entity/Cliente.java
@@ -0,0 +1,89 @@
+package com.devlomose.springboot.data.jpa.app.models.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Cliente at: src/main/java/com/devlomose/springboot/data/jpa/app/models/entity
+ * Created by @DevLomoSE at 14/9/21 10:25.
+ */
+@Entity
+@Table(name="clientes") //<- esto para modificar el nombre de la tabla en la BD
+public class Cliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+
+    @Column(name = "coreo") //<- esto para modificar el nombre de la columna en la tabla
+    private String email;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name="created_at",
+            nullable = false)
+    private Date createdAt;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name="updated_at",
+            updatable = true,
+            columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private Date updatedAt;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
