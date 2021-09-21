c38b4e303174f96c0adf2db5e8717c9bb0d5eede
DevLomoSE <jonathancggs@gmail.com>
2021-09-21 10:58:01 -0500

    :bento: Service Layer
    adding the implemented class for the client service interface

---
+++ src/main/java/com/devlomose/springboot/data/jpa/app/models/service/ClienteServiceImplement.java
@@ -0,0 +1,8 @@
+package com.devlomose.springboot.data.jpa.app.models.service;
+
+/**
+ * ClienteServiceImplement at: src/main/java/com/devlomose/springboot/data/jpa/app/models/service
+ * Created by @DevLomoSE at 21/9/21 10:57.
+ */
+public class ClienteServiceImplement {
+}
