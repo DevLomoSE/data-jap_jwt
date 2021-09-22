5f175ec2e35ad0e610cc5940fc027e4ec0e917f6
DevLomoSE <jonathancggs@gmail.com>
2021-09-22 11:01:26 -0500

    🧩 Factura
    creating the factura entity class

---
+++ src/main/java/com/devlomose/springboot/data/jpa/app/models/entity/Factura.java
@@ -0,0 +1,5 @@
+package com.devlomose.springboot.data.jpa.app.models.entity;/**
+ * Factura at: src/main/java/com/devlomose/springboot/data/jpa/app/models/entity
+ * Created by @DevLomoSE at 22/9/21 10:46.
+ */public class Factura {
+}
