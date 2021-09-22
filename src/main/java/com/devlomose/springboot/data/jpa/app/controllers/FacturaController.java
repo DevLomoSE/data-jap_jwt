4de44865e73e14399d41fd90cee63a404532fd25
DevLomoSE <jonathancggs@gmail.com>
2021-09-22 12:34:33 -0500

    :bento: Factura Form
    creating the html template of the form to create factura

---
+++ src/main/java/com/devlomose/springboot/data/jpa/app/controllers/FacturaController.java
@@ -0,0 +1,50 @@
+package com.devlomose.springboot.data.jpa.app.controllers;
+
+import com.devlomose.springboot.data.jpa.app.models.entity.Cliente;
+import com.devlomose.springboot.data.jpa.app.models.entity.Factura;
+import com.devlomose.springboot.data.jpa.app.models.service.ClienteServiceImplement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Map;
+
+/**
+ * FacturaController at: src/main/java/com/devlomose/springboot/data/jpa/app/controllers
+ * Created by @DevLomoSE at 22/9/21 12:01.
+ */
+@Controller
+@RequestMapping("/factura")
+@SessionAttributes("factura")
+public class FacturaController {
+
+    @Autowired
+    private ClienteServiceImplement clienteService;
+
+
+     @GetMapping("/form/{IdCliente}")
+    public String create(@PathVariable(value="IdCliente") Long IdCliente,
+                         Map<String, Object> model,
+                         RedirectAttributes flash/*,
+                         SessionStatus sessionStatus*/) {
+        Cliente cliente = clienteService.findById(IdCliente);
+
+        if(cliente == null){
+            flash.addFlashAttribute("error", "El cliente solicitado no existe en la BD.");
+            return "redirect:/cliente/listado";
+        }
+
+         Factura factura = new Factura();
+        factura.setCliente(cliente);
+
+        model.put("factura", factura);
+        model.put("titulo", "Crear factura");
+
+        /*sessionStatus.setComplete();*/
+
+        return "facturas/form";
+    }
+
+}
