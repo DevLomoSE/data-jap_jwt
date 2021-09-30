4f46d25f2fb5c40c96eec71a874e895dad2b24ec
DevLomoSE <jonathancggs@gmail.com>
2021-09-29 19:01:08 -0500

    :recycle: refactor AUTH
    moving handler package into auth package

--- src/main/java/com/devlomose/springboot/data/jpa/app/auth/handler/LoginSuccessHandler.java
+++ src/main/java/com/devlomose/springboot/data/jpa/app/auth/handler/LoginSuccessHandler.java
@@ -1,4 +1,4 @@
-package com.devlomose.springboot.data.jpa.app.handler;
+package com.devlomose.springboot.data.jpa.app.auth.handler;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
